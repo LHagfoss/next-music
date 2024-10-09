65cd583cf150e57f5107c3bad71bdc8761ea156a
lagos <lucash.1707@gmail.com>
2024-10-09 23:24:58 +0200

    first update

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import MusicPlayer from "@/components/MusicPlayer";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-[100vw] h-[100vh] p-5">
+      <MusicPlayer />
+    </div>
+  );
+}
---
+++ src/songs/songs.js
@@ -0,0 +1,12 @@
+import song1Cover from "@/songs/song1.jpg"
+import song1 from "@/songs/song1.wav"
+
+export const songs = [
+    {
+      id: 1,
+      title: "Penis Song",
+      artist: "Random Artist",
+      url: `${song1}`,
+      cover: `${song1Cover}`,
+    },
+  ];
